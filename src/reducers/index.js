c630ce17695559b2fde82638368cdc064cd1dd56
Rishabh <mailrishabh1403@gmail.com>
2017-09-05 21:25:08 +0530

    complete reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-
+import weather from './reducer_weather';
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: weather 
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,8 @@
+export default function(state = [],action){
+    switch (action.type) {
+        case 'FETCH_WEATHER':
+            // return state.concat([ action.payload.data ]);
+            return [ action.payload.data, ...state];
+    }
+    return state;
+}
