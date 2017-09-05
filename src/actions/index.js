2217557ea8e789e40f3f354307e17cba90cd8119
Rishabh <mailrishabh1403@gmail.com>
2017-09-05 20:40:46 +0530

    include axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
 let API_KEY = 'd2b7f04c47fccbdd30aa79ac90b68c08';
+const Root_Url = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${Root_Url}&q=${city},us`;
+    return {
+        type : FETCH_WEATHER
+    }
+}
